8ce9d6fc28316c5d5b9f11c8c9e167a1b43cf5e9
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-02-04 12:32:01 +0900

    Update Append-and-Delete.cpp

--- Append-and-Delete.cpp
+++ Append-and-Delete.cpp
@@ -1,3 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'appendAndDelete' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. STRING t
+ *  3. INTEGER k
+ */
 
 string appendAndDelete(string s, string t, int k)
 {
